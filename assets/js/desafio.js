a9fab9dd29c98581ee651a7c522ab580ffee2be8
stefanypedrosa <stefany.pedrosa@fatec.sp.gov.br>
2019-12-04 10:43:24 -0300

    js

---
+++ assets/js/desafio.js
@@ -0,0 +1,29 @@
+function pegaItem(evt){
+    evt.dataTransfer.setData("TAREFA",evt.target.id);
+}
+
+function habilitaArrastar(evt){
+    evt.preventDefault();
+}
+
+function recebeItem(evt){
+    var idTarefa = evt.dataTransfer.getData("TAREFA");
+    var tarefa = document.getElementById(idTarefa);
+    evt.target.appendChild(tarefa);
+}
+
+function buscaTarefas(){
+    fetch("http://www.professorisidro.com.br/tarefas.php",{"method":"GET"})
+      .then(response => response.json())
+      .then(json => mostraTarefa(json));
+}
+
+function mostraTarefa(){
+    var qtde = document.getElementById("qtde").value;
+    var task;
+    var num=1;
+    for(i=0; i<qtde; i++){
+        task = json[i];
+        document.getElementById("pendentes").innerHTML += '<div class="itemTarefa" draggable="true" ondragstart="pegaItem(event);" id="t'+num+'">'+task+'</div>';
+    }
+}
